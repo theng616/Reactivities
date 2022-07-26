b31aaea1124367f3ef94a2297c31cd51ad9bb7d2
Jason Wu <jasonwu616@gmail.com>
2022-07-26 16:20:56 +0800

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,8 +19,20 @@ export default class ActivityStore {
 		);
 	}
 
+	get groupedActivites() {
+		return Object.entries(
+			this.activitiesByDate.reduce((activities, activity) => {
+				const date = activity.date;
+				activities[date] = activities[date]
+					? [...activities[date], activity]
+					: [activity];
+				return activities;
+			}, {} as {[key: string]: Activity[]})
+		);
+	}
+
 	loadActivities = async () => {
-        this.loadingInitial = true;
+		this.loadingInitial = true;
 		try {
 			const activities = await agent.Activities.list();
 			activities.forEach((activity) => {
@@ -32,37 +44,36 @@ export default class ActivityStore {
 		}
 	};
 
-    loadActivity = async (id: string) => {
-        let activity = this.getActivity(id);
-        if (activity) {
-            this.selectedActivity = activity;
-            return activity;
-        } else {
-            this.loadingInitial =true;
-            try {
-                activity = await agent.Activities.details(id);
-                this.setActivity(activity);
-                runInAction(()=> {
-                    this.selectedActivity = activity;
-                })
-                this.setLoadingInitial(false);
-                return activity;
-            }
-            catch (error) {
-                console.log (error);
-                this.setLoadingInitial(false);
-            }
-        }
-    }
+	loadActivity = async (id: string) => {
+		let activity = this.getActivity(id);
+		if (activity) {
+			this.selectedActivity = activity;
+			return activity;
+		} else {
+			this.loadingInitial = true;
+			try {
+				activity = await agent.Activities.details(id);
+				this.setActivity(activity);
+				runInAction(() => {
+					this.selectedActivity = activity;
+				});
+				this.setLoadingInitial(false);
+				return activity;
+			} catch (error) {
+				console.log(error);
+				this.setLoadingInitial(false);
+			}
+		}
+	};
 
-    private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split("T")[0];
+	private setActivity = (activity: Activity) => {
+		activity.date = activity.date.split("T")[0];
 		this.activityRegistry.set(activity.id, activity);
-    }
+	};
 
-    private getActivity = (id: string) => {
-        return this.activityRegistry.get(id)
-    }
+	private getActivity = (id: string) => {
+		return this.activityRegistry.get(id);
+	};
 
 	setLoadingInitial = (state: boolean) => {
 		this.loadingInitial = state;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,11 +3,12 @@ import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilter from "./ActivityFilter";
 import ActivityList from "./ActivityList";
 
 export default observer(function ActivityDashboard() {
 	const { activityStore } = useStore();
-    const { loadActivities, activityRegistry } = activityStore; 
+	const { loadActivities, activityRegistry } = activityStore;
 
 	useEffect(() => {
 		if (activityRegistry.size <= 1) loadActivities();
@@ -18,12 +19,13 @@ export default observer(function ActivityDashboard() {
 
 	return (
 		<Grid>
-			<Grid.Column width="10">
+			<Grid.Column width="2" />
+			<Grid.Column width="8">
 				<ActivityList />
 			</Grid.Column>
 			<Grid.Column width="6">
-                <h2>Activity Filter</h2>
+				<ActivityFilter />
 			</Grid.Column>
 		</Grid>
 	);
-})
+});
